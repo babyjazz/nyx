fb6c4493ffba5de36f46419db6283666ac8d922f
100x <100x@100xs-MacBook-Pro.local>
2025-03-20 21:17:25 +0700

    feat: add tokens list

--- src/components/common/myTokens/tokenTable.jsx
+++ src/components/common/myTokens/tokenTable.jsx
@@ -1,8 +1,47 @@
-function TokenTable() {
+import { formatUnits } from "viem";
+
+function TokenTable({ tokens, isLoading }) {
+  if (isLoading) {
+    return (
+      <div className='flex flex-col items-center justify-center w-full h-full py-10 gap-3'>
+        <h1 className='text-white'>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (tokens.length === 0) {
+    return (
+      <div className='flex flex-col items-center justify-center w-full h-full py-10 gap-3'>
+        <h1 className='text-white'>You have no token</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Token Table</h1>
-    </div>
+    <table className='text-white text-left'>
+      <thead>
+        <tr>
+          <th>Token</th>
+          <th>Token Address</th>
+          <th>Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokens.map((token) => (
+          <tr key={token.token_address}>
+            <td>{token.name}</td>
+            <td>{token.token_address}</td>
+            <td>
+              {/* {Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(formatUnits(token.balance, token.decimals))} */}
+              ${formatUnits(token.balance, token.decimals)}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
--- src/components/common/myTokens/tokenView.jsx
+++ src/components/common/myTokens/tokenView.jsx
@@ -1,99 +1,119 @@
 import React, { useState, useEffect } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import { useReadContract } from "wagmi";
-// import tokenStakingPoolABI from "../../assets/abi/tokenstakingpool.json";
 import TokenTable from "./tokenTable";
-import { useAccount, useBalance } from "wagmi";
+import { useAccount, useBalance, useChainId } from "wagmi";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
 
 function TokenView() {
   const { isConnected, address } = useAccount();
+  const chainId = useChainId();
+  const [tokens, setTokens] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { data: balance, isLoading: isBalanceLoading } = useBalance({
+    address,
+    chainId,
+  });
 
-  // const {
-  //   data: amountToken,
-  //   isError,
-  //   isLoading,
-  //   isFetched,
-  //   status,
-  // } = useReadContract({
-  //   abi: tokenStakingPoolABI,
-  //   address: tokenStakingPoolContracts[0],
-  //   functionName: "amountStaked",
-  //   args: ["0x8Db2Ec88263267d025dED29Db1049D13B9D221b7"],
-  //   watch: true,
-  // });
+  const getTokens = async () => {
+    try {
+      await Moralis.start({
+        // can move to .env
+        apiKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjcxMTM5OWU3LTRmYzgtNDUwMC1hZWY2LWMxNjc1MmM5YjUyMSIsIm9yZ0lkIjoiNDM3MTE5IiwidXNlcklkIjoiNDQ5NjgzIiwidHlwZUlkIjoiOGZmYTA0YjgtMTg5Mi00Yzg5LThhZGMtYjAzYjFlMTRkZjIxIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NDIzOTczNDUsImV4cCI6NDg5ODE1NzM0NX0.UX2DiwUJndPFHFZGwgyNOpoZaZMsIQg2Hp2DUefQkMM",
+      });
 
-  // if (!isLoading && !isError) {
-  //   console.log("The counter value is ", amountToken.toString());
-  // }
+      /** We can map network for supporting more networks. or find another solution */
+      const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+        // chain: EvmChain.BASE, // specify your chain for test
+        chain: chainId,
+        address,
+      });
 
-  // useEffect(() => {
-  //   console.log(isFetched, isLoading, status);
-  //   if (isFetched) {
-  //     console.log(amountToken);
-  //   }
-  // }, [isFetched]);
+      const _tokens = response.jsonResponse.filter(
+        (token) => token.verified_contract
+      );
+      if (balance) {
+        _tokens.push({
+          name: "ETH",
+          token_address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+          balance: balance.value,
+          decimals: balance.decimals,
+        });
+      }
+      setTokens(_tokens);
+    } catch (e) {
+      console.error("debug er ", e);
+    }
+    setIsLoading(false);
+  };
 
-  // useEffect(() => {
-  //   setAmountStakedToken(amountToken);
-  // }, [amountToken]);
+  useEffect(() => {
+    if (isConnected && !!address && !isBalanceLoading) {
+      getTokens();
+    }
+  }, [isConnected, address, isBalanceLoading]);
 
   return (
     <div className='font-[RobotoMono] flex flex-col gap-5'>
       <div>
         <h1 className='lg:text-[23px] text-lg font-extrabold   bg-gradient-to-r from-[#5AB0FF] to-[#01FFC2] inline-block text-transparent bg-clip-text'>
-          Tokens (0)
+          Tokens ({tokens.length})
         </h1>
       </div>
       <div className='w-full border border-slate-500 rounded-xl lg:px-6 px-3 py-5 relative bg-black bg-opacity-75 flex flex-col gap-5'>
-        <div className='flex flex-col items-center justify-center w-full h-full py-10 gap-3'>
-          <img
-            src='./assets/images/icon/crypto_wallet.png'
-            alt='None'
-            className='md:h-24 h-16 w-auto'
-          />
-          <h1 className='text-center text-xl font-semibold text-gray-100'>
-            {isConnected ? "You have no token" : "Please connect wallet"}
-          </h1>
-          <ConnectButton.Custom>
-            {({
-              account,
-              chain,
-              openConnectModal,
-              authenticationStatus,
-              mounted,
-            }) => {
-              // Note: If your app doesn't use authentication, you
-              // can remove all 'authenticationStatus' checks
-              const ready = mounted && authenticationStatus !== "loading";
-              const connected =
-                ready &&
-                account &&
-                chain &&
-                (!authenticationStatus ||
-                  authenticationStatus === "authenticated");
+        {isConnected ? (
+          <TokenTable tokens={tokens} isLoading={isLoading} />
+        ) : (
+          <div className='flex flex-col items-center justify-center w-full h-full py-10 gap-3'>
+            <img
+              src='./assets/images/icon/crypto_wallet.png'
+              alt='None'
+              className='md:h-24 h-16 w-auto'
+            />
+            <h1 className='text-center text-xl font-semibold text-gray-100'>
+              Please connect wallet
+            </h1>
 
-              return (
-                <div>
-                  {(() => {
-                    if (!connected) {
-                      return (
-                        <button
-                          className='w-[170px] clipButton font-[Nippo] h-[40px] text-[15px]'
-                          onClick={openConnectModal}
-                          type='button'
-                        >
-                          Connect Wallet
-                        </button>
-                      );
-                    }
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+              }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted && authenticationStatus !== "loading";
+                const connected =
+                  ready &&
+                  account &&
+                  chain &&
+                  (!authenticationStatus ||
+                    authenticationStatus === "authenticated");
 
-                    return <TokenTable />;
-                  })()}
-                </div>
-              );
-            }}
-          </ConnectButton.Custom>
-        </div>
+                return (
+                  <div>
+                    {(() => {
+                      if (!connected) {
+                        return (
+                          <button
+                            className='w-[170px] clipButton font-[Nippo] h-[40px] text-[15px]'
+                            onClick={openConnectModal}
+                            type='button'
+                          >
+                            Connect Wallet
+                          </button>
+                        );
+                      }
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
+          </div>
+        )}
       </div>
     </div>
   );
